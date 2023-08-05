22f3dc346bd3e814b9e5325711ca67cf7db58cfe
AntonStrebkov <wancler987@gmail.com>
2023-08-05 19:46:15 +0700

    add homework 1

---
+++ hw1/script.js
@@ -0,0 +1,41 @@
+/*
+1) Дан массив const arr = [1, 5, 7, 9] с помощью Math.min и spread оператора, найти минимальное число в массиве, решение задание должно состоять из одной строки
+2) Напишите функцию createCounter, которая создает счетчик и возвращает объект с двумя методами: increment и decrement. Метод increment должен увеличивать значение счетчика на 1, а метод decrement должен уменьшать значение счетчика на 1. Значение счетчика должно быть доступно только через методы объекта, а не напрямую.
+
+3) Напишем функцию, которая будет находить факториал числа с использованием рекурсии:
+// примеры вызова функции
+console.log(factorial(5)); // выводит 120 (5 * 4 * 3 * 2 * 1)
+console.log(factorial(0)); // выводит 1 (по определению факториала)
+*/
+
+// Задача 1
+const arr = [1, 5, 7, 9];
+console.log(Math.min(...arr));
+
+// Задача 2
+const createCounter = (number) => {
+  return {
+    increment() {
+      console.log(number + 1);
+    },
+    decrement() {
+      console.log(number - 1);
+    },
+  };
+};
+
+const counter = createCounter(6);
+counter.decrement(counter);
+counter.increment(counter);
+
+// Задача 3
+
+function factorial(number) {
+  if (number === 1) {
+    return 1;
+  }
+  factorial(number - 1) * number;
+}
+
+console.log(factorial(5)); // выводит 120 (5 * 4 * 3 * 2 * 1)
+console.log(factorial(0)); // выводит 1 (по определению факториала)
